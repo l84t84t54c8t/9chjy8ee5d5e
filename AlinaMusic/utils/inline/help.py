a97afd4f1c4f05bf85fbd2e0a82a3607b0382696
l888lmn <deakoking85@gmail.com>
2024-10-11 23:11:20 +0300

    Update help.py

--- AlinaMusic/utils/inline/help.py
+++ AlinaMusic/utils/inline/help.py
@@ -1,15 +1,7 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from YukkiMusic import app
 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from AlinaMusic import app
 from config import SUPPORT_GROUP
 
 
