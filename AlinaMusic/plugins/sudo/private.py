13a338fa99990dbd6ea1ba8f203691d8a26c2d9d
l888lmn <deakoking85@gmail.com>
2024-10-12 00:55:01 +0300

    Update private.py

--- AlinaMusic/plugins/sudo/private.py
+++ AlinaMusic/plugins/sudo/private.py
@@ -11,15 +11,15 @@
 from pyrogram import filters
 from pyrogram.types import Message
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import (
+from AlinaMusic import app
+from AlinaMusic.misc import SUDOERS
+from AlinaMusic.utils.database import (
     add_private_chat,
     get_private_served_chats,
     is_served_private_chat,
     remove_private_chat,
 )
-from YukkiMusic.utils.decorators.language import language
+from AlinaMusic.utils.decorators.language import language
 
 import config
 
