288455e13059a74c410d5369bc3a155ab680546a
l888lmn <deakoking85@gmail.com>
2024-10-11 22:57:34 +0300

    Update Soundcloud.py

--- AlinaMusic/platforms/Soundcloud.py
+++ AlinaMusic/platforms/Soundcloud.py
@@ -11,7 +11,7 @@
 from os import path
 
 from yt_dlp import YoutubeDL
-from YukkiMusic.utils.formatters import seconds_to_min
+from AlinaMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
