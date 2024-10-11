349b6462d0d27682dcf83201396cad7d4db3f18f
l888lmn <deakoking85@gmail.com>
2024-10-11 22:30:15 +0300

    Add files via upload

---
+++ AlinaMusic/plugins/misc/autoleave.py
@@ -0,0 +1,114 @@
+#
+# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
+#
+# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
+# and is released under the MIT License.
+# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+import asyncio
+
+from pyrogram.enums import ChatType
+from datetime import datetime
+
+import config
+from YukkiMusic import app
+from YukkiMusic.core.call import Yukki
+from YukkiMusic.utils.database import (
+    get_assistant,
+    get_client,
+    is_active_chat,
+    is_autoend,
+)
+
+
+autoend = {}
+
+
+async def auto_leave():
+    if config.AUTO_LEAVING_ASSISTANT == str(True):
+        from YukkiMusic.core.userbot import assistants
+
+        async def leave_inactive_chats(client):
+            left = 0
+            try:
+                async for i in client.get_dialogs():
+                    chat_type = i.chat.type
+                    if chat_type in [
+                        ChatType.SUPERGROUP,
+                        ChatType.GROUP,
+                        ChatType.CHANNEL,
+                    ]:
+                        chat_id = i.chat.id
+                        if chat_id not in [
+                            config.LOG_GROUP_ID,
+                            -1002159045835,
+                            -1002146211959,
+                        ]:
+                            if left == 20:
+                                break
+                            if not await is_active_chat(chat_id):
+                                try:
+                                    await client.leave_chat(chat_id)
+                                    left += 1
+                                except:
+                                    continue
+            except:
+                pass
+
+        while not await asyncio.sleep(config.AUTO_LEAVE_ASSISTANT_TIME):
+            tasks = []
+            for num in assistants:
+                client = await get_client(num)
+                tasks.append(leave_inactive_chats(client))
+
+            # Using asyncio.gather for running the leave_inactive_chats and same time for all assistant
+            await asyncio.gather(*tasks)
+
+
+async def auto_end():
+    while True:
+        await asyncio.sleep(30)
+        if not await is_autoend():
+            continue 
+        for chat_id, timer in list(autoend.items()):
+            if datetime.now() > timer:
+                if not await is_active_chat(chat_id):
+                    del autoend[chat_id]
+                    continue
+
+                userbot = await get_assistant(chat_id)
+                members = []
+         
+                try:
+                    async for member in userbot.get_call_members(chat_id):
+                        if member is None:
+                            continue
+                        members.append(member)
+                except ValueError:
+                    try:
+                        await Yukki.stop_stream(chat_id)
+                    except Exception:
+                        pass
+                    continue
+                if len(members) <= 1:
+                    try:
+                        await Yukki.stop_stream(chat_id)
+                    except Exception:
+                        pass
+
+                    try:
+                        await app.send_message(
+                            chat_id,
+                            "ʙᴏᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴄʟᴇᴀʀᴇᴅ ᴛʜᴇ ǫᴜᴇᴜᴇ ᴀɴᴅ ʟᴇғᴛ ᴠɪᴅᴇᴏᴄʜᴀᴛ ʙᴇᴄᴀᴜsᴇ ɴᴏ ᴏɴᴇ ᴡᴀs ʟɪsᴛᴇɴɪɴɢ sᴏɴɢs ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+                        )
+                    except Exception:
+                        pass
+
+                del autoend[chat_id]
+
+
+asyncio.create_task(auto_leave())
+asyncio.create_task(auto_end())
---
+++ AlinaMusic/plugins/tools/active.py
@@ -0,0 +1,106 @@
+#
+# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
+#
+# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
+# and is released under the MIT License.
+# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+from pyrogram import filters
+from pyrogram.errors import ChannelInvalid
+from pyrogram.types import Message
+
+from strings import get_command
+from YukkiMusic import app
+from YukkiMusic.misc import SUDOERS, db
+from YukkiMusic.utils.database.memorydatabase import (
+    get_active_chats,
+    get_active_video_chats,
+    remove_active_chat,
+    remove_active_video_chat,
+)
+
+# Commands
+ACTIVEVC_COMMAND = get_command("ACTIVEVC_COMMAND")
+ACTIVEVIDEO_COMMAND = get_command("ACTIVEVIDEO_COMMAND")
+
+
+# Function for removing the Active voice and video chat also clear the db dictionary for the chat
+async def _clear_(chat_id):
+    db[chat_id] = []
+    await remove_active_video_chat(chat_id)
+    await remove_active_chat(chat_id)
+
+
+@app.on_message(filters.command(ACTIVEVC_COMMAND) & SUDOERS)
+async def activevc(_, message: Message):
+    mystic = await message.reply_text("ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ....ᴘʟᴇᴀsᴇ ʜᴏʟᴅ ᴏɴ")
+    served_chats = await get_active_chats()
+    text = ""
+    j = 0
+    for x in served_chats:
+        try:
+            title = (await app.get_chat(x)).title
+            if (await app.get_chat(x)).username:
+                user = (await app.get_chat(x)).username
+                text += f"<b>{j + 1}.</b>  [{title}](https://t.me/{user})[`{x}`]\n"
+            else:
+                text += f"<b>{j + 1}. {title}</b> [`{x}`]\n"
+            j += 1
+        except ChannelInvalid:
+            await _clear_(x)
+            continue
+    if not text:
+        await mystic.edit_text("ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ's")
+    else:
+        await mystic.edit_text(
+            f"**ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ's:-**\n\n{text}",
+            disable_web_page_preview=True,
+        )
+
+
+@app.on_message(filters.command(ACTIVEVIDEO_COMMAND) & SUDOERS)
+async def activevi_(_, message: Message):
+    mystic = await message.reply_text("ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ....ᴘʟᴇᴀsᴇ ʜᴏʟᴅ ᴏɴ")
+    served_chats = await get_active_video_chats()
+    text = ""
+    j = 0
+    for x in served_chats:
+        try:
+            title = (await app.get_chat(x)).title
+            if (await app.get_chat(x)).username:
+                user = (await app.get_chat(x)).username
+                text += f"<b>{j + 1}.</b>  [{title}](https://t.me/{user})[`{x}`]\n"
+            else:
+                text += f"<b>{j + 1}. {title}</b> [`{x}`]\n"
+            j += 1
+        except ChannelInvalid:
+            await _clear_(x)
+            continue
+    if not text:
+        await mystic.edit_text("ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛ's")
+    else:
+        await mystic.edit_text(
+            f"**ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛ's:-**\n\n{text}",
+            disable_web_page_preview=True,
+        )
+
+
+@app.on_message(filters.command(["ac"]) & SUDOERS)
+async def vc(client, message: Message):
+    ac_audio = str(len(await get_active_chats()))
+
+    await message.reply_text(f"ᴀᴄᴛɪᴠᴇ ᴄʜᴀᴛs ɪɴғᴏ: {ac_audio}")
+
+
+__MODULE__ = "Acᴛɪᴠᴇ"
+__HELP__ = """
+<b>✧ /ac</b> - Cʜᴇᴄᴋ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ᴏɴ ʙᴏᴛ.
+
+<b>✧ /activevoice</b> - Cʜᴇᴄᴋ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ᴀɴᴅ ᴠɪᴅᴇᴏ ᴄᴀʟʟs ᴏɴ ʙᴏᴛ.
+
+<b>✧ /activevideo</b> - Cʜᴇᴄᴋ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄᴀʟʟs ᴏɴ ʙᴏᴛ.
+
+<b>✧ /stats</b> - Cʜᴇᴄᴋ Bᴏᴛs Sᴛᴀᴛs
+"""
