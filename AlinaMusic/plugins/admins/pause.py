666e77c02f7b41b8894cb107b1d6213d9b2db58e
l888lmn <deakoking85@gmail.com>
2024-10-12 00:17:44 +0300

    Update pause.py

--- AlinaMusic/plugins/admins/pause.py
+++ AlinaMusic/plugins/admins/pause.py
@@ -9,20 +9,23 @@
 #
 
 from pyrogram import filters
-from pyrogram.types import Message
-from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils.database import is_music_playing, music_off
-from YukkiMusic.utils.decorators import AdminRightsCheck
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from AlinaMusic import app
+from AlinaMusic.core.call import Alina
+from AlinaMusic.utils.database import is_music_playing, music_off
+from AlinaMusic.utils.decorators import AdminRightsCheck
 
 from config import BANNED_USERS
 
-# Commands
-PAUSE_COMMAND = get_command("PAUSE_COMMAND")
 
 
-@app.on_message(filters.command(PAUSE_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(
+    filters.command(
+        ["pause", "cpause", "وسبە", "وەستانی کاتی"], prefixes=["/", "@", ".", "#", ""]
+    )
+    & ~filters.private
+    & ~BANNED_USERS
+)
 @AdminRightsCheck
 async def pause_admin(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
@@ -30,5 +33,33 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Yukki.pause_stream(chat_id)
-    await message.reply_text(_["admin_2"].format(message.from_user.mention))
+    await Alina.pause_stream(chat_id)
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="▷ 𝖱𝖾𝗌𝗎𝗆𝖾", callback_data=f"ADMIN Resume|{chat_id}"
+            ),
+            InlineKeyboardButton(
+                text="𝖱𝖾𝗉𝗅𝖺𝗒 ↺", callback_data=f"ADMIN Replay|{chat_id}"
+            ),
+        ],
+    ]
+    await message.reply_text(_["admin_2"].format(message.from_user.mention),
+    reply_markup=InlineKeyboardMarkup(buttons),
+    )
+
+
+
+__MODULE__ = "Pause"
+__HELP__ = """
+**Pause Music**
+
+This module allows administrators to pause the music playback in the group.
+
+Commands:
+- /pause: Pause the music playback in groups.
+- /cpause: Pause the music playback in channels.
+
+Note:
+- Only administrators can use these commands.
+"""
