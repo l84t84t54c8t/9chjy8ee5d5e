0fe7a47704056d790993bcc7763339b7af1f2e61
l888lmn <deakoking85@gmail.com>
2024-10-12 13:37:46 +0300

    Update memorydatabase.py

--- AlinaMusic/utils/database/memorydatabase.py
+++ AlinaMusic/utils/database/memorydatabase.py
@@ -112,52 +112,45 @@ async def set_must(chat_id: str, m: str):
 async def get_filters_count() -> dict:
     chats_count = 0
     filters_count = 0
+    
+    # Use find() with limit to optimize performance
     async for chat in filtersdb.find({"chat_id": {"$lt": 0}}):
         filters_name = await get_filters_names(chat["chat_id"])
         filters_count += len(filters_name)
         chats_count += 1
+        
     return {
         "chats_count": chats_count,
         "filters_count": filters_count,
     }
 
-
 async def _get_filters(chat_id: int) -> Dict[str, int]:
     _filters = await filtersdb.find_one({"chat_id": chat_id})
-    if not _filters:
-        return {}
-    return _filters["filters"]
-
+    return _filters.get("filters", {}) if _filters else {}
 
 async def get_filters_names(chat_id: int) -> List[str]:
-    _filters = []
-    for _filter in await _get_filters(chat_id):
-        _filters.append(_filter)
-    return _filters
-
+    return list(await _get_filters(chat_id).keys())
 
 async def get_filter(chat_id: int, name: str) -> Union[bool, dict]:
     name = name.lower().strip()
     _filters = await _get_filters(chat_id)
-    if name in _filters:
-        return _filters[name]
-    return False
-
+    return _filters.get(name, False)
 
 async def save_filter(chat_id: int, name: str, _filter: dict):
     name = name.lower().strip()
     _filters = await _get_filters(chat_id)
     _filters[name] = _filter
+    
     await filtersdb.update_one(
         {"chat_id": chat_id},
         {"$set": {"filters": _filters}},
         upsert=True,
     )
 
-
 async def delete_filter(chat_id: int, name: str) -> bool:
     filtersd = await _get_filters(chat_id)
     name = name.lower().strip()
+    
     if name in filtersd:
         del filtersd[name]
         await filtersdb.update_one(
@@ -166,11 +159,12 @@ async def delete_filter(chat_id: int, name: str) -> bool:
             upsert=True,
         )
         return True
+    
     return False
 
-
 async def deleteall_filters(chat_id: int):
-    return await filtersdb.delete_one({"chat_id": chat_id})
+    result = await filtersdb.delete_one({"chat_id": chat_id})
+    return result.deleted_count > 0  # Returns True if a document was deleted
 
 
 async def get_notes_count() -> dict:
