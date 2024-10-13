ba7bdb9ab75a72b449c4c169519f0d39335e644d
l888lmn <deakoking85@gmail.com>
2024-10-13 21:04:02 +0300

    Update time.py

--- AlinaMusic/plugins/USERBOT/time.py
+++ AlinaMusic/plugins/USERBOT/time.py
@@ -1,17 +1,23 @@
 import pyrogram
+from pyrogram import Client
 from pytz import timezone
 from datetime import datetime
 
 target_timezone = timezone('Asia/Baghdad')
 
-def change_profile_name():
-    all = client.get_me()
-    firstNmae = all.first_name if all.first_name else ""
+def change_profile_name(client):
+    # Fetch the current user details
+    all = app.get_me()
+    firstName = all.first_name if all.first_name else ""
     lastName = all.last_name if all.last_name else ""
-    allName = firstNmae + " " + lastName
+    allName = f"{firstName} {lastName}".strip()  # Combine first and last name, avoid extra space
+
+    # Get the current time in the target timezone
     current_time = datetime.now(target_timezone).strftime("%I:%M %p")
+
+    # Update the profile name with the current time
     new_name = f"{allName} - {current_time}"
     client.update_profile(first_name=new_name)
-  
-        change_profile_name()
-        pyrogram.idle()
+
+change_profile_name()
+pyrogram.idle()
