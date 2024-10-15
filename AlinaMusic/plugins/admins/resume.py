328a7b79bb4d0a8febe9cb5b17f02a60f507a4f6
l888lmn <deakoking85@gmail.com>
2024-10-15 15:44:18 +0300

    Update resume.py

--- AlinaMusic/plugins/admins/resume.py
+++ AlinaMusic/plugins/admins/resume.py
@@ -1,3 +1,4 @@
+
 #
 # Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
 #
@@ -11,11 +12,12 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
+from config import BANNED_USERS
 from AlinaMusic import app
 from AlinaMusic.core.call import Alina
 from AlinaMusic.utils.database import is_music_playing, music_on
 from AlinaMusic.utils.decorators import AdminRightsCheck
-from config import BANNED_USERS
+
 
 
 @app.on_message(
@@ -33,7 +35,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await Alina.resume_stream(chat_id)
+    await Yukki.resume_stream(chat_id)
     buttons_resume = [
         [
             InlineKeyboardButton(
@@ -52,18 +54,3 @@ async def resume_com(cli, message: Message, _, chat_id):
         _["admin_4"].format(message.from_user.mention),
         reply_markup=InlineKeyboardMarkup(buttons_resume),
     )
-
-
-__MODULE__ = "Resume"
-__HELP__ = """
-**Resume**
-
-This module allows administrators to resume playback of the currently paused track.
-
-Commands:
-- /resume: Resumes playback of the currently paused track for group.
-- /cresume: Resumes playback of the currently paused track for channel.
-
-Note:
-- Only administrators can use these commands.
-"""
