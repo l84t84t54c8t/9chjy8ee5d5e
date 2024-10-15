c2d36ff249ca790a775c3584db4a8fbc3c3e0d8c
l888lmn <deakoking85@gmail.com>
2024-10-15 15:43:58 +0300

    Update pause.py

--- AlinaMusic/plugins/admins/pause.py
+++ AlinaMusic/plugins/admins/pause.py
@@ -1,3 +1,4 @@
+
 #
 # Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
 #
@@ -11,11 +12,12 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
+from config import BANNED_USERS
 from AlinaMusic import app
 from AlinaMusic.core.call import Alina
 from AlinaMusic.utils.database import is_music_playing, music_off
 from AlinaMusic.utils.decorators import AdminRightsCheck
-from config import BANNED_USERS
+
 
 
 @app.on_message(
@@ -43,22 +45,6 @@ async def pause_admin(cli, message: Message, _, chat_id):
             ),
         ],
     ]
-    await message.reply_text(
-        _["admin_2"].format(message.from_user.mention),
-        reply_markup=InlineKeyboardMarkup(buttons),
+    await message.reply_text(_["admin_2"].format(message.from_user.mention),
+    reply_markup=InlineKeyboardMarkup(buttons),
     )
-
-
-__MODULE__ = "Pause"
-__HELP__ = """
-**Pause Music**
-
-This module allows administrators to pause the music playback in the group.
-
-Commands:
-- /pause: Pause the music playback in groups.
-- /cpause: Pause the music playback in channels.
-
-Note:
-- Only administrators can use these commands.
-"""
