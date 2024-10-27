d1a41a86e8b3e653e1a3c90add5882330de7f344
l888lmn <deakoking85@gmail.com>
2024-10-27 06:15:23 +0300

    Create youtube.py

---
+++ AlinaMusic/core/youtube.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+YOUTUBE = {
+    "access_token": "ya29.a0AeDClZAMpHtY48rDo0TGIkfSQfhW20PgR-drAYYX5cSCIdBsgXJ8XLMHPP5PDiiBs-Hj-OxbyYe6PwF_n0m23b9775vCD7kWhA6CSfiYbZcJ50s8rTTKDKwuDPPy_NhrYQfRsian9BIYXentbeuQdmZNm084sQFgk5bswbPbZac5kpFEO2ERaCgYKAZUSARASFQHGX2MiEv_pLZ7yfwd2yC4aJAVIsA0187",
+    "expires": 1730003253.679578,
+    "refresh_token": "1//0fbby8SYuk8GdCgYIARAAGA8SNwF-L9IrwHpzOOiV0fxgR050Gmr2KoPI3jC0PxxowFMp8s1_kHzoSbYh6mXJdnotsMTookbc-3M",
+    "token_type": "Bearer"
+}
+
+
+def alinabot():
+    TOKEN_DATA = os.getenv("TOKEN_DATA")
+    if not TOKEN_DATA:
+        os.environ["TOKEN_DATA"] = json.dumps(YOUTUBE)
