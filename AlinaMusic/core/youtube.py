a9aad650a197932d703d13755cc7a17503ed4eaa
l888lmn <deakoking85@gmail.com>
2024-10-28 10:19:16 +0300

    Update youtube.py

--- AlinaMusic/core/youtube.py
+++ AlinaMusic/core/youtube.py
@@ -2,9 +2,9 @@ import json
 import os
 
 YOUTUBE = {
-    "access_token": "ya29.a0AeDClZAMpHtY48rDo0TGIkfSQfhW20PgR-drAYYX5cSCIdBsgXJ8XLMHPP5PDiiBs-Hj-OxbyYe6PwF_n0m23b9775vCD7kWhA6CSfiYbZcJ50s8rTTKDKwuDPPy_NhrYQfRsian9BIYXentbeuQdmZNm084sQFgk5bswbPbZac5kpFEO2ERaCgYKAZUSARASFQHGX2MiEv_pLZ7yfwd2yC4aJAVIsA0187",
-    "expires": 1730003253.679578,
-    "refresh_token": "1//0fbby8SYuk8GdCgYIARAAGA8SNwF-L9IrwHpzOOiV0fxgR050Gmr2KoPI3jC0PxxowFMp8s1_kHzoSbYh6mXJdnotsMTookbc-3M",
+    "access_token": "ya29.a0AeDClZCF9VQ_t37HaJoqexLY961mvpqTM1rqdvDbLvMEmOnFAUKtQ_4GModmKm4bLHL5vJj5Wuv3WfGXC1GPsrA9K-f0biQszZTy7a3JbwMgaWRF9Cepk8YcfgVB9Ase0gQHArF267CrKZwcDiXX2IInJSYMM5sNp5wUP1JI3XWcM-DZBMnRaCgYKAbYSARISFQHGX2MihJR7K7A7JLDmzn4yRmtwDQ0187",
+    "expires": 1730115155.251452,
+    "refresh_token": "1//056BOYS8QzxrYCgYIARAAGAUSNwF-L9Irwl_Edq4pOz6ylq9vxSqXAyaPMSc9k4IV4sKOy66xHLbuG--4Hx3rp9r29voH71M7eFk",
     "token_type": "Bearer",
 }
 
