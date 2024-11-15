27e4a281e67ea30e0010ce51ee6540b6fa0f1e47
l888lmn <deakoking85@gmail.com>
2024-11-15 16:49:25 +0300

    Update bot.py

--- AlinaMusic/core/bot.py
+++ AlinaMusic/core/bot.py
@@ -16,20 +16,16 @@ import sys
 
 from pyrogram import Client
 from pyrogram.enums import ChatMemberStatus
-from pyrogram.errors import (
-    ChatSendPhotosForbidden,
-    ChatWriteForbidden,
-    FloodWait,
-    MessageIdInvalid,
-)
-from pyrogram.types import (
-    BotCommand,
-    BotCommandScopeAllChatAdministrators,
-    BotCommandScopeAllGroupChats,
-    BotCommandScopeAllPrivateChats,
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-)
+from pyrogram.types import BotCommand
+from pyrogram.types import BotCommandScopeAllChatAdministrators
+from pyrogram.types import BotCommandScopeAllGroupChats
+from pyrogram.types import BotCommandScopeAllPrivateChats
+from pyrogram.types import BotCommandScopeChat
+from pyrogram.types import BotCommandScopeChatMember, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import ChatSendPhotosForbidden
+from pyrogram.errors import ChatWriteForbidden
+from pyrogram.errors import FloodWait
+from pyrogram.errors import MessageIdInvalid
 
 import config
 
@@ -100,9 +96,8 @@ class AlinaBot(Client):
         get_me = await self.get_me()
         self.username = get_me.username
         self.id = get_me.id
-        self.name = self.me.first_name + " " + (self.me.last_name or "")
-        self.mention = self.me.mention
-        # Create the button
+        self.name = f"{get_me.first_name} {get_me.last_name or ''}"
+        self.mention = get_me.mention
         button = InlineKeyboardMarkup(
             [
                 [
@@ -117,111 +112,77 @@ class AlinaBot(Client):
         try:
             await self.send_message(
                 config.LOG_GROUP_ID,
-                text=f"<u><b>{self.mention} Bot Started :</b><u>\n\nId : <code>{self.id}</code>\nName : {self.name}\nUsername : @{self.username}",
+                text=(
+                    f"<u><b>{self.mention} Bot Started :</b></u>\n\n"
+                    f"Id : <code>{self.id}</code>\n"
+                    f"Name : {self.name}\n"
+                    f"Username : @{self.username}"
+                ),
                 reply_markup=button,
             )
-        except:
+        except Exception as e:
             LOGGER(__name__).error(
-                "Bot has failed to access the log group. Make sure that you have added your bot to your log channel and promoted as admin!"
+                "Bot failed to access the log group. Ensure the bot is added and promoted as admin."
             )
-            LOGGER(__name__).error("An error occurred", exc_info=True)
+            LOGGER(__name__).error("Error details:", exc_info=True)
             sys.exit()
+
         if config.SET_CMDS == str(True):
             try:
-                await self.set_bot_commands(
-                    commands=[
-                        BotCommand("start", "• دەستپێکردنی بۆت"),
-                        BotCommand("help", "• فەرمان و ڕوونکردنەوە"),
-                        BotCommand("ping", "• پشکنینی بۆت"),
-                    ],
-                    scope=BotCommandScopeAllPrivateChats(),
-                )
-                await self.set_bot_commands(
-                    commands=[
-                        BotCommand("start", "• دەستپێکردنی بۆت"),
-                        BotCommand("help", "• فەرمان و ڕوونکردنەوە"),
-                        BotCommand("ping", "• پشکنینی بۆت"),
-                        BotCommand("play", "• پەخشکردنی گۆرانی داواکراو"),
-                        BotCommand("vplay", "• پەخشکردنی ڤیدیۆی داواکراو"),
-                        BotCommand("stop", "• وەستاندنی پەخشکردن"),
-                        BotCommand("pause", "• وەستاندنی پەخشکردن بۆ ماوەیەکی کاتی"),
-                        BotCommand("resume", "• دەستپێکردنەوەی پەخشکردن"),
-                        BotCommand("queue", "• پشکنین بۆ گۆرانی ڕیزکراو"),
-                        BotCommand(
-                            "skip", "• تێپەڕاندنی گۆرانی-پەخشکردن بۆ گۆرانی دواتر"
-                        ),
-                        BotCommand("reload", "• ڕیلۆدکردنی بۆت"),
-                        BotCommand("end", "• وەستاندنی پەخشکردن"),
-                        BotCommand("song", "• داگرتنی گۆرانی"),
-                        BotCommand("video", "• داگرتنی ڤیدیۆ"),
-                        BotCommand("couples", "• دیاری کردن کەپڵ تەنیا بۆخۆشی"),
-                        BotCommand("cute", "• دیاری کردنی ڕێژەی قشتیت تەنیا یارییە"),
-                        BotCommand(
-                            "love", "• دیاری کردنی ڕێژەی خۆشەویستی نێوان دوو کەس"
-                        ),
-                        BotCommand("sudolist", "• لیستی گەشەپێدەرانی بۆت"),
-                        BotCommand("dev", "• خاوەنی بۆت"),
-                        BotCommand("staff", "• پیشاندانی ستافی ئەدمینەکان"),
-                        BotCommand("bots", "• پیشاندانی بۆتەکانی گرووپ"),
-                        BotCommand("botcheck", "• پشکنینی بۆتەکانی تێلەگرام"),
-                        BotCommand("gdata", "• زانیاری گرووپەکان"),
-                        BotCommand("stats", "• ئاماری بۆتی ئەلینا"),
-                        BotCommand("story", "• کۆنتڕۆڵ کردنی ناردنی ستۆری"),
-                        BotCommand("admin", "• بانگکردنی ئەدمینەکانی گرووپ"),
-                        BotCommand("gpt", "• چات جیپیتی"),
-                        BotCommand("gemini", "• زیرەکی دەستکردی گوگڵ"),
-                        BotCommand("tr", "• وەرگێڕانی دەق بۆ هەر زمانێك"),
-                        BotCommand("lyrics", "• هێنانی ژێرنووسی گۆرانی"),
-                    ],
-                    scope=BotCommandScopeAllGroupChats(),
-                )
-                await self.set_bot_commands(
-                    commands=[
-                        BotCommand("start", "• دەستپێکردنی بۆت"),
-                        BotCommand("ping", "• پشکنینی بۆت"),
-                        BotCommand("help", "• فەرمانەکان"),
-                        BotCommand("all", "• تاگکردنی ئەندامەکان بە دەق"),
-                        BotCommand("cancel", "• وەستاندنی تاگکردن"),
-                        BotCommand("settings", "• ڕێکخستی بۆت"),
-                        BotCommand("reload", "• ڕیلۆدکردنی بۆت"),
-                        BotCommand("play", "• پەخشکردنی گۆرانی داواکراو"),
-                        BotCommand("vplay", "• پەخشکردنی ڤیدیۆی داواکاراو"),
-                        BotCommand("pause", "• وەستاندنی پەخشکردن بۆ ماوەیەکی کاتی"),
-                        BotCommand("resume", "• دەستپێکردنەوەی پەخشکردن"),
-                        BotCommand("queue", "• پشکنین بۆ گۆرانی ڕیزکراو"),
-                        BotCommand(
-                            "skip", "• تێپەڕاندنی گۆرانی-پەخشکردن بۆ گۆرانی دواتر"
-                        ),
-                        BotCommand("end", "• وەستاندنی پەخشکردن"),
-                        BotCommand("playlist", "• لیستی گۆرانی-پەخشکراو"),
-                        BotCommand("stop", "• وەستاندنی پەخشکردن"),
-                        BotCommand("lyrics", "• هێنانی دەقی گۆرانی"),
-                        BotCommand("song", "• داگرتنی گۆرانی"),
-                        BotCommand("video", "• داگرتنی ڤیدیۆ"),
-                        BotCommand("couples", "• دیاری کردن کەپڵ تەنیا بۆخۆشی"),
-                        BotCommand("cute", "• دیاری کردنی ڕێژەی قشتیت تەنیا یارییە"),
-                        BotCommand(
-                            "love", "• دیاری کردنی ڕێژەی خۆشەویستی نێوان دوو کەس"
-                        ),
-                        BotCommand("sudolist", "• لیستی گەشەپێدەرانی بۆت"),
-                        BotCommand("dev", "• خاوەنی بۆت"),
-                        BotCommand("filter", "• زیادکردنی چات"),
-                        BotCommand("lang", "• گۆڕینی زمانی بۆت"),
-                        BotCommand("welcome", "• دانانی بەخێرهاتن"),
-                        BotCommand("staff", "• پیشاندانی ستافی ئەدمینەکان"),
-                        BotCommand("bots", "• پیشاندانی بۆتەکانی گرووپ"),
-                        BotCommand("botcheck", "• پشکنینی بۆتەکانی تێلەگرام"),
-                        BotCommand("gdata", "• زانیاری گرووپەکان"),
-                        BotCommand("stats", "• ئاماری بۆتی ئەلینا"),
-                        BotCommand(
-                            "story", "• بۆ داخستن و کردنەوەی ناردنی ستۆری لە گرووپ"
-                        ),
-                        BotCommand(
-                            "forward", "• بۆ داخستن و کردنەوەی ناردنی ڕێکڵام لە گرووپ"
-                        ),
-                        BotCommand("admin", "• بانگکردنی ئەدمینەکانی گرووپ"),
-                        BotCommand("gpt", "• چات جیپیتی"),
-                        BotCommand("gemini", "• زیرەکی دەستکردی گوگڵ"),
+                await self._set_default_commands()
+            except Exception as e:
+                LOGGER(__name__).warning("Failed to set commands:", exc_info=True)
+
+    async def _set_default_commands(self):
+        private_commands = [
+            BotCommand("start", "• دەستپێکردنی بۆت"),
+            BotCommand("help", "• فەرمان و ڕوونکردنەوە"),
+            BotCommand("ping", "• پشکنینی بۆت"),
+        ]
+        group_commands = [
+            BotCommand("start", "• دەستپێکردنی بۆت"),
+            BotCommand("help", "• فەرمان و ڕوونکردنەوە"),
+            BotCommand("ping", "• پشکنینی بۆت"),
+            BotCommand("play", "• پەخشکردنی گۆرانی داواکراو"),
+            BotCommand("vplay", "• پەخشکردنی ڤیدیۆی داواکراو"),
+            BotCommand("pause", "• وەستاندنی پەخشکردن بۆ ماوەیەکی کاتی"),
+            BotCommand("resume", "• دەستپێکردنەوەی پەخشکردن"),
+            BotCommand("skip", "• تێپەڕاندنی گۆرانی-پەخشکردن بۆ گۆرانی دواتر"),
+            BotCommand("end", "• وەستاندنی پەخشکردن"),
+            BotCommand("stop", "• وەستاندنی پەخشکردن"),
+            BotCommand("admin", "• بانگکردنی ئەدمینەکانی گرووپ"),
+            BotCommand("gpt", "• چات جیپیتی"),
+            BotCommand("gemini", "• زیرەکی دەستکردی گوگڵ"),
+            BotCommand("tr", "• وەرگێڕانی دەق بۆ هەر زمانێك"),
+            BotCommand("couples", "• دیاری کردن کەپڵ تەنیا بۆخۆشی"),
+            BotCommand("cute", "• دیاری کردنی ڕێژەی قشتیت تەنیا یارییە"),
+            BotCommand("sudolist", "• لیستی گەشەپێدەرانی بۆت"),
+            BotCommand("dev", "• خاوەنی بۆت"),
+            BotCommand("staff", "• پیشاندانی ستافی ئەدمینەکان"),
+            BotCommand("quran", "• پەخشکردنی قورئانی پیرۆز"),
+        ]
+        admin_commands = [
+            BotCommand("start", "• دەستپێکردنی بۆت"),
+            BotCommand("ping", "• پشکنینی بۆت"),
+            BotCommand("help", "• فەرمانەکان"),
+            BotCommand("play", "• پەخشکردنی گۆرانی داواکراو"),
+            BotCommand("vplay", "• پەخشکردنی ڤیدیۆی داواکاراو"),
+            BotCommand("playlist", "• لیستی گۆرانی-پەخشکراو"),
+            BotCommand("quran", "• پەخشکردنی قورئانی پیرۆز"),
+            BotCommand("pause", "• وەستاندنی پەخشکردن بۆ ماوەیەکی کاتی"),
+            BotCommand("resume", "• دەستپێکردنەوەی پەخشکردن"),
+            BotCommand("skip", "• تێپەڕاندنی گۆرانی-پەخشکردن بۆ گۆرانی دواتر"),
+            BotCommand("end", "• وەستاندنی پەخشکردن"),
+            BotCommand("stop", "• وەستاندنی پەخشکردن"),
+            BotCommand("playmode", "• گۆڕینی پەخشکردن"),
+            BotCommand("settings", "• ڕێکخستی بۆت"),
+            BotCommand("all", "• تاگکردنی ئەندامەکان بە دەق"),
+            BotCommand("cancel", "• وەستاندنی تاگکردن"),
+            BotCommand("couples", "• دیاری کردن کەپڵ تەنیا بۆخۆشی"),
+            BotCommand("cute", "• دیاری کردنی ڕێژەی قشتیت تەنیا یارییە"),
+            BotCommand("admin", "• بانگکردنی ئەدمینەکانی گرووپ"),
+            BotCommand("gpt", "• چات جیپیتی"),
+            BotCommand("gemini", "• زیرەکی دەستکردی گوگڵ"),
                         BotCommand("tr", "• وەرگێڕانی دەق بۆ هەر زمانێك"),
                         BotCommand("ban", "• دەرکردنی-باندکردنی کەسێك"),
                         BotCommand("unban", "• لادانی باند-دەرکردنی لەسەر کەسێك"),
@@ -231,18 +192,58 @@ class AlinaBot(Client):
                         BotCommand("promote", "• زیادکردنی ئەدمین"),
                         BotCommand("demote", "• لادانی ئەدمین"),
                         BotCommand("purge", "• سڕینەوەی چاتی گرووپ بە ڕیپلەی"),
-                        BotCommand("open", "• کردنەوەی تێلی گرووپ"),
-                        BotCommand("close", "• داخستنی تێلی گرووپ"),
-                        BotCommand("playmode", "• گۆڕینی پەخشکردن"),
-                        BotCommand(
-                            "quran",
-                            "• پەخشکردنی قورئانی پیرۆز",
-                        ),
-                    ],
-                    scope=BotCommandScopeAllChatAdministrators(),
-                )
-            except:
-                pass
+             BotCommand("sudolist", "• لیستی گەشەپێدەرانی بۆت"),
+                        BotCommand("dev", "• خاوەنی بۆت"),
+                        BotCommand("filter", "• زیادکردنی چات"),
+                        BotCommand("lang", "• گۆڕینی زمانی بۆت"),
+                        BotCommand("welcome", "• دانانی بەخێرهاتن"),
+                        BotCommand("staff", "• پیشاندانی ستافی ئەدمینەکان"),
+                        BotCommand("bots", "• پیشاندانی بۆتەکانی گرووپ"),
+                        BotCommand("botcheck", "• پشکنینی بۆتەکانی تێلەگرام"),
+                        BotCommand("gdata", "• زانیاری گرووپەکان"),
+                        BotCommand("stats", "• ئاماری بۆتی ئەلینا"),
+                        BotCommand("story", "• بۆ داخستن و کردنەوەی ناردنی ستۆری لە گرووپ"),
+                        BotCommand("forward", "• بۆ داخستن و کردنەوەی ناردنی ڕێکڵام لە گرووپ"),
+        ]
+        owner_commands = [
+            BotCommand("update", "Update the bot"),
+            BotCommand("restart", "Restart the bot"),
+            BotCommand("logs", "Get logs"),
+            BotCommand("export", "Export all data of mongodb"),
+            BotCommand("import", "Import all data in mongodb"),
+            BotCommand("addsudo", "Add a user as a sudoer"),
+            BotCommand("delsudo", "Remove a user from sudoers"),
+            BotCommand("sudolist", "List all sudo users"),
+            BotCommand("log", "Get the bot logs"),
+            BotCommand("getvar", "Get a specific environment variable"),
+            BotCommand("delvar", "Delete a specific environment variable"),
+            BotCommand("setvar", "Set a specific environment variable"),
+            BotCommand("usage", "Get dyno usage information"),
+            BotCommand("maintenance", "Enable or disable maintenance mode"),
+            BotCommand("logger", "Enable or disable logging"),
+            BotCommand("block", "Block a user"),
+            BotCommand("unblock", "Unblock a user"),
+            BotCommand("blacklist", "Blacklist a chat"),
+            BotCommand("whitelist", "Whitelist a chat"),
+            BotCommand("blacklisted", "List all blacklisted chats"),
+            BotCommand("autoend", "Enable or disable auto end for streams"),
+            BotCommand("reboot", "Reboot the bot"),
+            BotCommand("restart", "Restart the bot"),
+        ]
+
+        await self.set_bot_commands(private_commands, scope=BotCommandScopeAllPrivateChats())
+        await self.set_bot_commands(group_commands, scope=BotCommandScopeAllGroupChats())
+        await self.set_bot_commands(admin_commands, scope=BotCommandScopeAllChatAdministrators())
+
+        LOG_GROUP_ID = f"@{config.LOG_GROUP_ID}" if isinstance(config.LOG_GROUP_ID, str) and not config.LOG_GROUP_ID.startswith("@") else config.LOG_GROUP_ID
+
+        for owner_id in config.OWNER_ID:
+            try:
+                await self.set_bot_commands(owner_commands, scope=BotCommandScopeChatMember(chat_id=LOG_GROUP_ID, user_id=owner_id))
+                await self.set_bot_commands(owner_commands, scope=BotCommandScopeChat(chat_id=owner_id))
+            except Exception as e:
+                LOGGER(__name__).warning("Failed to set owner commands for user %s:", owner_id, exc_info=True)
+
         else:
             pass
         try:
@@ -257,6 +258,3 @@ class AlinaBot(Client):
         else:
             self.name = get_me.first_name
         LOGGER(__name__).info(f"MusicBot started as {self.name}")
-
-    async def stop(self):
-        await super().stop()
